d8e10160a9df777cef9c30b57f1d388d14207d0f
Gangsta <sashika25@mail.ru>
2017-06-01 02:53:58 +0300

    Клетка игры

---
+++ src/gui/Cell.java
@@ -0,0 +1,47 @@
+package gui;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import logic.ClassForGame;
+
+public class Cell extends Pane {
+    private int x;
+    private int y;
+
+
+    private Text move = new Text();
+
+    public int getPositionRow() {
+        return x;
+    }
+
+    public int getPositionColumn() {
+        return y;
+    }
+
+    public void setMove(ClassForGame.Move move) {
+        switch (move) {
+            case X:
+                this.move.setText("X");
+                break;
+            case O:
+                this.move.setText("O");
+                break;
+            default:
+                break;
+        }
+    }
+
+    public Cell(int h) {
+        move.setText(String.valueOf(h));
+        this.x = x;
+        this.y = y;
+        getChildren().addAll(move);
+        setPrefSize(50, 50);
+
+    }
+}
+
+
